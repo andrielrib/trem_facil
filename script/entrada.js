07b913b445a963405649cd2cc88ce2a888ff79fb
Andriel Ribeiro <andriel_ribeiro@estudante.sesisenai.org.br>
2025-06-12 10:12:15 -0300

    add-entrada validada

---
+++ script/entrada.js
@@ -0,0 +1,7 @@
+ document.getElementById('btn_entrar').addEventListener('click', function () {
+            window.location.href = "login.html"; 
+        });
+
+        document.getElementById('btn_cadastrar').addEventListener('click', function () {
+            window.location.href = 'cadastro1.html';
+        });
