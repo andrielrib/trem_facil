1c93a69ff076541757f088fc78b9962bae16ef58
Arthur Farias <arthur_s_farias@estudante.sesisenai.org.br>
2025-06-13 09:14:06 -0300

    add - tela incial

--- script/perfil.js
+++ script/perfil.js
@@ -1,19 +1,21 @@
-document.getElementById('M.A.R.S.').addEventListener('click', function () {
-        window.location.href = "tela_inicial.html"; 
-    });
+function telaInicial(){
+    window.location.href = "tela_inicial.html";
+
+}
+
 
 document.getElementById('A.U.R.A.').addEventListener('click', function () {
-        window.location.href = "sensor.html"; 
-    });
+    window.location.href = "sensor.html";
+});
 
 document.getElementById('P.I.V.O.T.').addEventListener('click', function () {
-        window.location.href = "pesquisa.html"; 
-    });
+    window.location.href = "pesquisa.html";
+});
 
 document.getElementById('E.C.H.O.').addEventListener('click', function () {
-        window.location.href = "suporte_alerta.html"; 
-    });
+    window.location.href = "suporte_alerta.html";
+});
 
 document.getElementById('S.T.E.L.A.').addEventListener('click', function () {
-        window.location.href = "perfil.html"; 
-    });
+    window.location.href = "perfil.html";
+});
