5fb2b4bb39f5b78e804ad2953d820e780e3e6253
Arthur Farias <arthur_s_farias@estudante.sesisenai.org.br>
2025-06-13 09:17:33 -0300

    add - onclick sensor

--- script/perfil.js
+++ script/perfil.js
@@ -1,21 +1,4 @@
 function telaInicial(){
     window.location.href = "tela_inicial.html";
-
 }
 
-
-document.getElementById('A.U.R.A.').addEventListener('click', function () {
-    window.location.href = "sensor.html";
-});
-
-document.getElementById('P.I.V.O.T.').addEventListener('click', function () {
-    window.location.href = "pesquisa.html";
-});
-
-document.getElementById('E.C.H.O.').addEventListener('click', function () {
-    window.location.href = "suporte_alerta.html";
-});
-
-document.getElementById('S.T.E.L.A.').addEventListener('click', function () {
-    window.location.href = "perfil.html";
-});
