497dc60ebe1f18ea6d1b5edf78454196e5d60e59
Andriel Ribeiro <andriel_ribeiro@estudante.sesisenai.org.br>
2025-06-13 08:43:27 -0300

    add- comiit

--- script/esqueci_senha.js
+++ script/esqueci_senha.js
@@ -4,6 +4,7 @@ function reenviarCodigo() {
     if (/^\d{4,}$/.test(codigo)) {
         console.log("Código digitado:", codigo);
         window.location.href = 'redefenir_senha.html';
+        
     } else {
         alert("Por favor, digite pelo menos 4 números.");
     }
