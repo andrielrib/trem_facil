89bfc42036f997ff2ee6824fe567b3ad82ba151a
Arthur Farias <arthur_s_farias@estudante.sesisenai.org.br>
2025-09-16 10:09:45 -0300

    add - db

--- db.sql
+++ db.sql
@@ -1,5 +1,3 @@
-/*Trem fácil*/
-
 CREATE DATABASE trem_facil
 
 USE trem_facil 
@@ -21,32 +19,28 @@ CREATE TABLE  sensores    (
 id_sensores INT AUTO_INCREMENT PRIMARY KEY,
 nome_sensor VARCHAR(120) UNIQUE NOT NULL,
 data_sensor TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-FKid_trens  INT,
-FOREIGN KEY (FKid_trens) REFERENCES trens (id_trens)
 );
 
-
-
+CREATE TABLE  rotas    (
+id_rotas INT AUTO_INCREMENT PRIMARY KEY,
+nome_rotas VARCHAR(120) UNIQUE NOT NULL,
+data_rotas TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+FKid_sensores  INT,
+FOREIGN KEY (FKid_sensores) REFERENCES sensores (id_sensores)
+);
 
 CREATE TABLE trens (
 id_trens INT AUTO_INCREMENT PRIMARY KEY,
+nome_tren VARCHAR(120) UNIQUE NOT NULL,
+horario TIME UNIQUE NOT NULL,
+parada_tren VARCHAR(120) UNIQUE NOT NULL
+);
+
+CREATE TABLE estacoes (
+id_estacoes INT AUTO_INCREMENT PRIMARY KEY,
+nome_estacao VARCHAR(120) UNIQUE NOT NULL,
+FKid_trens  INT,
+FOREIGN KEY (FKid_trens) REFERENCES trens (id_trens)
 
 
-
-
-
-)
-
-
-
-
-
-
-
-
-
-CREATE TABLE rotas (
-id_rotas
-
-
-)
+);
