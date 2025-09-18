4345ac0d4dddcd8f051294a27d2df777727c398e
Andriel Ribeiro <andriel_ribeiro@estudante.sesisenai.org.br>
2025-09-18 08:51:11 -0300

    add> removendo imagens e iniciando a tela PAGINA_INCIAL

--- public/pagina_inicial.php
+++ public/pagina_inicial.php
@@ -1,28 +1,42 @@
-<html lang="en">
+<!DOCTYPE html>
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pagina inicial</title>
+    <title>Trem Fácil</title>
+    <link rel="stylesheet" href="../style/style.css">
 </head>
 <body>
-    
+    <div class="container">
 
+        <div class="logo">
+            <img src="../assets/icons/trem_bala_icon.png" alt="Logo Trem Fácil">
+            <h2>Trem <span>Fácil</span></h2>
+        </div>
 
+        <h3 class="bemvindo">BEM VINDO(A),<br><span>(Nome usuário)</span></h3>
 
+        <div class="menu">
+            <a href="#" class="card">
+                <img src="../assets/icons/perfil.png" alt="Perfil">
+                <p>PERFIL</p>
+            </a>
 
+            <a href="#" class="card">
+                <img src="../assets/icons/trens.png.png" alt="Trens">
+                <p>TRENS</p>
+            </a>
 
+            <a href="#" class="card">
+                <img src="../assets/icons/sensores.png" alt="Sensores">
+                <p>SENSORES</p>
+            </a>
 
-
-
-
-
-
-
-
-
-
-
-
-
+            <a href="#" class="card">
+                <img src="../assets/icons/estacoes.png.png" alt="Estações">
+                <p>ESTAÇÕES</p>
+            </a>
+        </div>
+    </div>
 </body>
 </html>
