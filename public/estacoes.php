946499cb815289122976e35fdf1bc6d1a469970a
Rafael-Almeida23 <rafael_a_de-mello@estudante.sesisenai.org.br>
2025-09-18 09:56:55 -0300

    add - hoje

--- public/estacoes.php
+++ public/estacoes.php
@@ -3,8 +3,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>estações</title>
+    <link rel="stylesheet" href="../style/style.css">
 </head>
 <body>
-    <dialog></dialog>
+
+<div class="img_entrada">
+        <img src="../assets/icons/estações.png" alt="Estações" width="150" height="50">
+    </div>
+
+    <br><br><br>
+
+    <div class="text_entrada">
+        <h1>Jà possui conta?</h1>
+    </div>
+    
+    <br><br>
+
+    <div class="alinhar_entrada">
+
+        <a href="login.php"><button id="btn_entrar" class="caixa_azul_entrada"><h2>Entrar</h2></button></a>
+
+        <br><br>
+
+        <a href="cadastro1.php"><button id="btn_cadastrar" class="caixa_azul_entrada"><h2>Cadastrar</h2></button></a>
+
+    </div>
+
+
 </body>
 </html>
