280d21eef0252e2c13e7427466dec2ff817af009
Andriel Ribeiro <andriel_ribeiro@estudante.sesisenai.org.br>
2025-09-18 09:00:31 -0300

    add>pagina inicial melhorias

--- public/pagina_inicial.php
+++ public/pagina_inicial.php
@@ -1,42 +1,43 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Trem Fácil</title>
-    <link rel="stylesheet" href="../style/style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Trem Fácil</title>
+  <link rel="stylesheet" href="../style/style.css">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
 </head>
 <body>
-    <div class="container">
+  <div class="container">
 
-        <div class="logo">
-            <img src="../assets/icons/trem_bala_icon.png" alt="Logo Trem Fácil">
-            <h2>Trem <span>Fácil</span></h2>
-        </div>
-
-        <h3 class="bemvindo">BEM VINDO(A),<br><span>(Nome usuário)</span></h3>
-
-        <div class="menu">
-            <a href="#" class="card">
-                <img src="../assets/icons/perfil.png" alt="Perfil">
-                <p>PERFIL</p>
-            </a>
-
-            <a href="#" class="card">
-                <img src="../assets/icons/trens.png.png" alt="Trens">
-                <p>TRENS</p>
-            </a>
-
-            <a href="#" class="card">
-                <img src="../assets/icons/sensores.png" alt="Sensores">
-                <p>SENSORES</p>
-            </a>
-
-            <a href="#" class="card">
-                <img src="../assets/icons/estacoes.png.png" alt="Estações">
-                <p>ESTAÇÕES</p>
-            </a>
-        </div>
+    <div class="logo">
+      <img src="../assets/icons/trem_bala_icon.png" alt="Logo Trem Fácil">
+      <h2>Trem <span>Fácil</span></h2>
     </div>
+
+    <h3 class="bemvindo">BEM VINDO(A),<br><span></span></h3>
+
+    <div class="menu">
+      <a href="#" class="card">
+        <img src="../assets/icons/perfil.png" alt="Perfil">
+  
+      </a>
+
+      <a href="#" class="card">
+        <img src="../assets/icons/trens.png.png" alt="Trens">
+     
+      </a>
+
+      <a href="#" class="card">
+        <img src="../assets/icons/sensores.png" alt="Sensores">
+       
+      </a>
+
+      <a href="#" class="card">
+        <img src="../assets/icons/estacoes.png.png" alt="Estações">
+      
+      </a>
+    </div>
+  </div>
 </body>
 </html>
