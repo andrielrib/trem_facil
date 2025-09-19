7c264410c90109c7128bcfa00269f84c3cfee447
Andriel Ribeiro <andriel_ribeiro@estudante.sesisenai.org.br>
2025-09-19 09:58:53 -0300

    add>estacoes

--- public/estacoes.php
+++ public/estacoes.php
@@ -11,5 +11,18 @@
         <img src="../assets/icons/estações.png" alt="Estações" width="150" height="50">
     </div>
 
+<hr style="border: 2px solid #0549b1;">
+<div class="flex_sensor">
+    <strong><h1 class="sensor">Estação Costa e Silva</h1></strong>
+    <img src="../assets/img/seta_baixo.png" alt="seta para baixo" width="60" height="40" class="img_seta">
+</div>
+<hr style="border: 2px solid #0549b1;">
+<div class="flex_sensor">
+    <strong><h1 class="sensor">Estação Pirabeiraba</h1></strong>
+    <img src="../assets/img/seta_baixo.png" alt="seta para baixo" width="60" height="40" class="img_seta">
+
+
+<hr style="border: 2px solid #0549b1;">
+    
 </body>
 </html>
