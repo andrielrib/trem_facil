fafa1095a443875a2ad269e3d0a1df96e5a21127
Arthur Farias <arthur_s_farias@estudante.sesisenai.org.br>
2025-09-23 12:16:51 -0300

    add diferenças

--- public/estacoes.php
+++ public/estacoes.php
@@ -1,14 +1,26 @@
-<html lang="en">
+<!DOCTYPE html>
+<html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>estações</title>
-    <link rel="stylesheet" href="../style/style.css">
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Estações</title>
+  <link rel="stylesheet" href="../style/style.css" />
 </head>
 <body>
 
-    <img src="../assets/icons/seta_esquerda.png" alt="Estações" width="50" height="50">
-    
-    
+  <div class="flex_estacao">
+    <button class="btn_seta" aria-label="Voltar">
+      <img src="../assets/icons/seta_esquerda.png" alt="Seta apontando para esquerda" width="24" height="24" />
+    </button>
+    <button class="title">ESTAÇÕES</button>
+  </div>
+
+  <hr class="divisor"/>
+
+  <div class="search_container">
+    <img src="../assets/icons/icone_lupa.png" alt="Ícone de lupa preta pequena à esquerda do input" class="icon_search" />
+    <input type="search" placeholder="Pesquisar Estações" class="input_search" />
+  </div>
+
 </body>
 </html>
