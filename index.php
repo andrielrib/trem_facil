f46a4a3e6eaeb21f6bbc624e7e1a46aef27adcc1
Andriel Ribeiro <andriel_ribeiro@estudnte.sesisenai.org.br>
2025-09-23 12:32:27 -0300

    add>teste

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if ($tipo) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Entrada</title>
-    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="../style/style.css">
 </head>
 <body>
     <div class="img_entrada">
