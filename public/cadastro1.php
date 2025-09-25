c19e93b2235d3ad68a5e6dd2dcf7e69f5dfc9e7f
Rafael-Almeida23 <rafael_a_de-mello@estudante.sesisenai.org.br>
2025-09-25 09:44:36 -0300

    add

--- public/cadastro1.php
+++ public/cadastro1.php
@@ -102,8 +102,8 @@ function validateCPF($cpf)
 </head>
 
 <body>
-    <div class="img_carinha_redefinir">
-        <img src="../assets/icons/trem_bala_icon.png" alt="Ícone Trem" width="260" height="210" class="img_carinha_redefinir">
+    <div class="i">
+        <img src="../assets/icons/trem_bala_icon.png" alt="Ícone Trem" width="260" height="210" class="">
     </div>
     <br>
 
