ad292efbc700974e2696cc1afd51f6837ca6b673
Davi_conti <davi_conti@estudante.sesisenai.org.br>
2025-11-06 10:06:21 -0300

    add

--- public/db.php
+++ public/db.php
@@ -4,7 +4,7 @@ $servername = "localhost";
 $username = "root";
 $password = "root";
 $dbname = "trem_facil";
-$conn = new mysqli($servername,$username,$password,$db-name);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 
 if ($conn->connect_error) {
