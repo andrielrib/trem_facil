ebda02812d22b74a0329614ab21e186df50eb63a
Andriel Ribeiro <andriel_ribeiro@estudante.sesisenai.org.br>
2025-11-06 10:02:39 -0300

    add> new db

--- public/db.php
+++ public/db.php
@@ -4,9 +4,11 @@ $servername = "localhost";
 $username = "root";
 $password = "root";
 $dbname = "trem_facil";
-$conn = new mysqli($servername,$username,$password,$db-name);
 
+// Cria a conexão com o banco de dados
+$conn = new mysqli($servername, $username, $password, $dbname);
 
+// Verifica se a conexão falhou
 if ($conn->connect_error) {
     die("Conexao falhou: " . $conn->connect_error);
 }
